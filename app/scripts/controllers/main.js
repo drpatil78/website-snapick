197ce511ca81cadc00cd60d04be05ad7afb3bdfa
Abhishek Hingnikar <djdark88.7@gmail.com>
2014-09-02 04:44:19 +0530

    finished website v1

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,32 +13,24 @@ angular.module('websiteSnapickApp')
     $scope.featureTagLines = [
     	'Introducing the Photosphere',
         'Organize your photos smartly',
-    	'Enhance & Filter',
-        'Design',
-        
+        'Photo Sharing in built !',
+    	'Enhance & Filter'
     ];
     
     $scope.featurePhotos = [
         '/images/app/Cloud.png',
         '/images/app/catalog.png',
-        '/images/app/Cloud.png',
-        '/images/app/Cloud.png'
+        '/images/app/social.png',
+        '/images/app/filter.png',
     ];
     
     $scope.featureExplanations = [
-
         'Photosphere (TM) is the collection of all your photos in multiple cloud & social services, In one place. The smart photosphere you can access all your photos, without filling your device.',
         'Snapick organizes your library smartly, buidling Smart Folders(tm) these dynamic folders are built on content and make it easy to browse through photos.',
+        'Snapick comes with inbuilt social network, which allows you to share your photos with your friends &amp; family with ease. It also integrates with popular social networks and organizes the content automagically, so you never miss a moment ever stay social !.',
         'Snapick comes with simple yet powerful filters and editing tools to enhance your photos, whats more is using our incredible AI we provide automated suggestions that are content aware.',
-        'Snapick is designed from ground up to provide your photos the respect they deserve, the app adapts automatically with the content appearing fresh and appealing everytime in a brand new way !',
     ];
 
-    $scope.backgroundImages = [
-    	'hero2',
-    	'hero3',
-    	'hero4',
-    	'hero5'	
-    ];
 
     $scope.feedImages = [
     	{ url: '/images/top/frag3.png' },
@@ -63,21 +55,13 @@ angular.module('websiteSnapickApp')
         }
     ];
     $scope.state = {
-    	featureBackgroundStyle : {
-    		backgroundImage: ''
-    	},
     	featureTagLine: $scope.featureTagLines[0],
         activeSlide: 0
     }
 
-    function setBackgroundStyle(index){
-    	$scope.state.featureBackgroundStyle.backgroundImage = 'url(/images/backgrounds/' + $scope.backgroundImages[index] + '.jpg)';
-    }
-    setBackgroundStyle(0);
 
     function handleSlideChange(newVal, oldVal){
     	if( newVal !== oldVal ){
-    		setBackgroundStyle(newVal);
 		    $scope.state.featureTagLine = $scope.featureTagLines[newVal];
     	}
     }
